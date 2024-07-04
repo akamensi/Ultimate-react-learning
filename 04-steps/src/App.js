32acecdd64df60c8487538a67ef0cd3e83217d9e
akamensi <akez.muhammed@gmail.com>
2024-07-04 15:55:28 +0200

    Children Props

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -10,7 +10,6 @@ export default function App() {
   return (
     <div>
       <Steps />
-      <Steps />
     </div>
   );
 }
@@ -42,22 +41,30 @@ function Steps() {
             Step {step} : {messages[step - 1]}
           </p>
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            <Button
+              textColor={"#fff"}
+              bgColor={"#7950f2"}
               onClick={handlePrevious}
             >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-              // onMouseEnter={() => alert("test")}
-            >
-              Next
-            </button>
+              <span>⬅</span>Previous
+            </Button>
+            <Button textColor={"#fff"} bgColor={"#7950f2"} onClick={handleNext}>
+              Next<span>➡</span>
+            </Button>
           </div>
         </div>
       )}
     </div>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
