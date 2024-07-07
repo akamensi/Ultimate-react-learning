8990e50ae9fe43c1cf694b39a1c4952d35410f5a
akamensi <akez.muhammed@gmail.com>
2024-07-07 20:12:37 +0200

    State Update Batching

--- 07-how-react-works/src/Components/TabContent.js
+++ 07-how-react-works/src/Components/TabContent.js
@@ -4,8 +4,36 @@ const TabContent = ({ item }) => {
   const [showDetails, setShowDetails] = useState(true);
   const [likes, setLikes] = useState(0);
 
+  //To know how manytime the component will render after the eventHandler "handleUndo"
+  // so I can see the automatic batching of update multiple state like in the "handleUndo"
+  console.log("render");
+
   function handleInc() {
     setLikes(likes + 1);
+  }
+
+  /*   function handleTripleInc(){
+    setLikes(likes + 1);
+    console.log(likes);
+    setLikes(likes + 1);
+    setLikes(likes + 1);
+      } we need a callBack function so we push the state to update line by line like the next */
+
+  //each time that we set state based on the previous state or on the current state
+  //-we should always , always use a callBack function
+  function handleTripleInc() {
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+  }
+
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
   }
 
   return (
@@ -21,13 +49,13 @@ const TabContent = ({ item }) => {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
--- 07-how-react-works/src/index.js
+++ 07-how-react-works/src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+//import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+//REACT 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+//so we can see the diffrent of the automatic Batching in REACT 18
+//REACT 17(No Automatic BATCHING)
+/* ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+); */
