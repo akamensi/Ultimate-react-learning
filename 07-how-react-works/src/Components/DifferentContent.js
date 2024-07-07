019f619ba4a1fb6acaf382ea284e66eebda7320a
akamensi <akez.muhammed@gmail.com>
2024-07-07 17:45:18 +0200

    RenderPhase-CommitPhase-Diffing

--- 07-how-react-works/src/Components/DifferentContent.js
+++ 07-how-react-works/src/Components/DifferentContent.js
@@ -4,6 +4,7 @@ const DifferentContent = () => {
   return (
     <div className="tab-content">
       <h4>I'm a DIFFERENT tab, so I reset state 💣💥</h4>
+      <img src="/Reacp-from-Trigger-to-Update-UI.png" alt="Recap" />
     </div>
   );
 };
